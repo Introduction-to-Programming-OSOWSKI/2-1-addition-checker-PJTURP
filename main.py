92ae5c8f72911fa3cdc9b5cf6a681258336c0c81
PJTURP <821042@mystma.org>
2021-09-28 09:01:09 -0500

    attempt one

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE HERE
+#def additionChecker
+def additionChecker(x,y,z):
+    if x + y == z:
+        return True
+
+    else:
+        return False
+#run function
+print(additionChecker(10, 12, 22))
